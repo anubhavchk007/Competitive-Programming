853867e7547a6a2e6c285f1f68eafb017276dbcb
anubhavchk007 <96805904+anubhavchk007@users.noreply.github.com>
2024-07-28 19:55:49 +0530

    Create functions.cpp

---
+++ Templates/functions.cpp
@@ -0,0 +1,14 @@
+int lcm(int a, int b){return (a / __gcd(a, b)) * b;}
+bool isprime(int n){if(n == 1 || n == 0) return 0; if(!(n & 1) && n > 2) return false; for(int i = 3; i * i <= n; i += 2){ if(n % i == 0) return false;} return true;};
+bool ispowerof2(int n){return (n!=0) &&  (!(n & (n-1)));};
+vector<int> sieve(int n) {int*arr = new int[n + 1](); vector<int> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
+int power(int a, int b, int mod) {int res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+int mul_inverse(int a, int m){return power(a, m - 2, m);};
+void extendgcd(int a, int b, int*v) {if (b == 0) {v[0] = 1; v[1] = 0; v[2] = a; return ;} extendgcd(b, a % b, v); int x = v[1]; v[1] = v[0] - v[1] * (a / b); v[0] = x; return;} //pass an arry of size1 3
+int mminv(int a, int b) {int arr[3]; extendgcd(a, b, arr); return arr[0];}  //for non prime b
+int mminvprime(int a, int b) {return power(a, b - 2, b);}
+int combination(int n, int r, int m, int *fact, int *ifact) {int val1 = fact[n]; int val2 = ifact[n - r]; int val3 = ifact[r]; return (((val1 * val2) % m) * val3) % m;}
+int mod_add(int a, int b, int m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+int mod_mul(int a, int b, int m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+int mod_sub(int a, int b, int m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+int mod_div(int a, int b, int m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}  //only for prime m 
